b70033f12a14b82a3fc7320840467ae6addcbdac
Rich Harris <richard.a.harris@gmail.com>
2018-05-20 23:58:25 -0400

    move file-watching into a worker

---
+++ workers/watch-directory.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const chokidar = require('chokidar');
+
+const watchers = {};
+
+process.on('message', event => {
+	const { dir, command } = event;
+
+	if (command === 'start-watching') {
+		const watcher = chokidar.watch(path.join(dir, '*'), {
+			ignoreInitial: true
+		});
+
+		const handler = () => {
+			process.send({
+				type: 'directory-changed',
+				dir
+			});
+		};
+
+		watcher.on('add', handler);
+		watcher.on('unlink', handler);
+
+		if (watchers[dir]) {
+			throw new Error(`Watcher already exists`);
+		}
+
+		watchers[dir] = watcher;
+	}
+
+	if (command === 'stop-watching') {
+		const watcher = watchers[dir];
+		if (!watcher) {
+			throw new Error(`Watcher does not exist`);
+		}
+
+		watcher.close();
+		watchers[dir] = null;
+	}
+});
